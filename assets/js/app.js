ea166d83825b58b87b2eb033e598901161c83312
MrsPepa <jose.leon1996@gmail.com>
2017-12-12 13:02:44 -0300

    actualizando archivos

---
+++ assets/js/app.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  setTimeout(function() {
+    $(".food").fadeOut(1500);
+  },3000);
+});
+$(document).ready(function() {   
+    setTimeout(function() {
+        $(".contenidoInicial").fadeIn(1500);
+    },3000);
+});
