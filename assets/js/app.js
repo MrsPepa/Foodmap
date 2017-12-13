8f04c6fba40677621f4e1adbd948f5500b0869b2
MrsPepa <jose.leon1996@gmail.com>
2017-12-13 12:02:48 -0300

    probando modal

--- assets/js/app.js
+++ assets/js/app.js
@@ -32,5 +32,12 @@ $(document).ready(function() {
     $('#japonesa').hide();
     $('#chilena').hide();
   }
-
+  $('#modal1').on('shown.bs.modal', function () {
+  $('#myInput').focus()
+})
 });
+$('.img1').mouseover(function(){
+  	$('.img-overplay').show().text('Dorayaki');
+  }).mouseout(function(){
+  	/*$('.img-overplay').hide();*/
+  })
