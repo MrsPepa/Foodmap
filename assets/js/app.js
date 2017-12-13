c2330ad6b58ee7e3cdb6af9013f8b5e35864938f
MrsPepa <jose.leon1996@gmail.com>
2017-12-13 14:07:41 -0300

    actualizando modal

--- assets/js/app.js
+++ assets/js/app.js
@@ -32,12 +32,84 @@ $(document).ready(function() {
     $('#japonesa').hide();
     $('#chilena').hide();
   }
-  $('#modal1').on('shown.bs.modal', function () {
+  $('#myModal').on('shown.bs.modal', function () {
   $('#myInput').focus()
 })
 });
+
+/*  efectos mouseover  */
 $('.img1').mouseover(function(){
-  	$('.img-overplay').show().text('Dorayaki');
-  }).mouseout(function(){
-  	/*$('.img-overplay').hide();*/
-  })
+	$('#overplay1').show();
+}).mouseout(function(){
+	$('#overplay1').hide();
+})
+$('.img2').mouseover(function(){
+	$('#overplay2').show();
+}).mouseout(function(){
+	$('#overplay2').hide();
+})
+$('.img3').mouseover(function(){
+	$('#overplay3').show();
+}).mouseout(function(){
+	$('#overplay3').hide();
+})
+$('.img4').mouseover(function(){
+	$('#overplay4').show();
+}).mouseout(function(){
+	$('#overplay4').hide();
+})
+$('.img5').mouseover(function(){
+	$('#overplay5').show();
+}).mouseout(function(){
+	$('#overplay5').hide();
+})
+$('.img6').mouseover(function(){
+	$('#overplay6').show();
+}).mouseout(function(){
+	$('#overplay6').hide();
+})
+$('.img7').mouseover(function(){
+	$('#overplay7').show();
+}).mouseout(function(){
+	$('#overplay7').hide();
+})
+$('.img8').mouseover(function(){
+	$('#overplay8').show();
+}).mouseout(function(){
+	$('#overplay8').hide();
+})
+$('.img9').mouseover(function(){
+	$('#overplay9').show();
+}).mouseout(function(){
+	$('#overplay9').hide();
+})
+$('.img10').mouseover(function(){
+	$('#overplay10').show();
+}).mouseout(function(){
+	$('#overplay10').hide();
+})
+$('.img11').mouseover(function(){
+	$('#overplay11').show();
+}).mouseout(function(){
+	$('#overplay11').hide();
+})
+$('.img12').mouseover(function(){
+	$('#overplay12').show();
+}).mouseout(function(){
+	$('#overplay12').hide();
+})
+$('.img13').mouseover(function(){
+	$('#overplay13').show();
+}).mouseout(function(){
+	$('#overplay13').hide();
+})
+$('.img14').mouseover(function(){
+	$('#overplay14').show();
+}).mouseout(function(){
+	$('#overplay14').hide();
+})
+$('.img15').mouseover(function(){
+	$('#overplay15').show();
+}).mouseout(function(){
+	$('#overplay15').hide();
+})
