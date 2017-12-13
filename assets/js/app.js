cbb9d80472255f614fcf353ca4e85a2c08a55737
MrsPepa <jose.leon1996@gmail.com>
2017-12-12 21:58:29 -0300

    actualizando dropdown y js

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,8 +3,34 @@ $(document).ready(function() {
     $(".food").fadeOut(1500);
   },3000);
 });
-$(document).ready(function() {   
-    setTimeout(function() {
-        $(".contenidoInicial").fadeIn(1500);
-    },3000);
+$(document).ready(function() {
+  setTimeout(function() {
+    $(".contenidoInicial").fadeIn(1500);
+  },3000);
+
+  $('.restaurante').hide();
+  $('.restaurante:first').show();
+
+  $('#chile').click(select);
+  $('#japon').click(select1);
+  $('#china').click(select2);
+
+  function select(){
+    $('#chilena').show();
+    $('#japonesa').hide();
+    $('#chinesse').hide();
+  }
+
+  function select1(){
+    $('#japonesa').show();
+    $('#chilena').hide();
+    $('#chinesse').hide();
+  }
+
+  function select2(){
+    $('#chinesse').show();
+    $('#japonesa').hide();
+    $('#chilena').hide();
+  }
+
 });
